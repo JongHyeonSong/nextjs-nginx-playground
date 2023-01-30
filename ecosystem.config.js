050d3abae6819d19b0b474d38307831132514ea4
sjh <mapelamll@naver.com>
2023-01-30 20:53:05 +0900

    에코 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,8 @@ module.exports = {
       repo : 'https://github.com/JongHyeonSong/nextjs-nginx-playground.git', 
       path : '/home/next-sjh.kro.kr/nextjs',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && npm run build &&  pm2 reload ecosystem.config.js --env production',
-      'pre-setup': 'echo $PWD >> ii2.txt',
+      'post-deploy' : 'echo $PWD >> ii2.txt && npm install && npm run build &&  pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
   }
